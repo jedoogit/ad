eb78d13727330857db50d57ed2f354a46261486c
jack doo <jekyungdoo@gmail.com>
2021-01-11 18:00:33 +0900

    지문인식

--- app/src/main/java/com/example/finger/MainActivity.java
+++ app/src/main/java/com/example/finger/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         private BiometricPrompt biometricPrompt;
         private BiometricPrompt.PromptInfo promptInfo;
 
-
+//ㅇㅇ
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
